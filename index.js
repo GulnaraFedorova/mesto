6b05517fd80378d1321746edb264afaa3e3cf6f0
Gulnara Fedorova <gul-khurmatova@yandex.ru>
2023-03-05 19:23:24 +0100

    4й спринт 1я итерация

--- index.js
+++ index.js
@@ -1,60 +1,29 @@
+let profileName = document.querySelector ('.profile__title');
+let profileInformation = document.querySelector ('.profile__text');
+let profileNameInput = document.querySelector ('.popup__form_name');
+let profileInformationInput = document.querySelector ('.popup__form_information');
+let popupCloseButton = document.querySelector ('.popup__close');
+let formElement = document.querySelector ('.popup__fieldset');
+let editPopup = document.querySelector ('.popup');
+let editPopupButton = document.querySelector('.profile__edit');
 
-let editPopup = document.querySelector('.popup');
- 
-let editPopupOpenButton = document.querySelector('.profile__edit');
-editPopupOpenButton.addEventListener('click', function () {
-    editPopup.classList.add('popup_opened');
-});
-
-let editPopupCloseButton = document.querySelector('.popup__close');
-editPopupCloseButton.addEventListener('click', function () {
-    editPopup.classList.remove('popup_opened');
-});
-
-function openPopup (popup) {
-    popup.classList. add ('popup_opened"');
+function openPopup () {
+    editPopup.classList. add ('popup_opened');
+    profileNameInput.value = profileName.textContent;
+    profileInformationInput.value = profileInformation.textContent;
 };
 
-function closePopup (popup) {
-    popup.classList.remove ('popup_opened');
+function closePopup () {
+    editPopup.classList.remove ('popup_opened');
 }
 
-let userName = 'Жак-Ив Кусто';
-let userInformation = 'Исследователь океана';
-
-let userNameElement = document.querySelector('.profile__title');
-userNameElement.textContent = userName;
-
-let userInformationElement = document.querySelector('.profile__text');
-userInformationElement.textContent = userInformation;
-
-let userNameInput = document.querySelector('.popup__form_name');
-userNameInput.value = userName;
-
-let userInformationInput = document.querySelector('.popup__form_information');
-userInformationInput.value = userInformation;
-
-
-userNameInput.addEventListener('input', function (evt) {
-    const value = evt.target.value;
-    userNameElement.textContent = value;
-});
-
-userInformationInput.addEventListener('input', function (evt) {
-    const value = evt.target.value;
-    userInformationElement.textContent = value;
-});
-
-
-
-let formElement = document.querySelector('.popup__fieldset');
 function formSubmitHandler (evt) {
 	evt.preventDefault(); 
-    let userNameInput = document.querySelector('.popup__form_name');
-    let userInformationInput = document.querySelector('.popup__form_information');
-    userName.textContent = userNameInput.value
-    userInformation.textContent = userInformationInput.value
+    profileName.textContent = profileNameInput.value;
+    profileInformation.textContent = profileInformationInput.value;
+    closePopup ();
 }
 
-
+editPopupButton.addEventListener ('click', openPopup);
+popupCloseButton.addEventListener ('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
