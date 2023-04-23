66520dbedab137cbe72ab799e1857eb34b051409
Gulnara Fedorova <gul-khurmatova@yandex.ru>
2023-04-23 17:05:57 +0200

    5й спринт 2я итерация

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,10 @@ const profileName = document.querySelector ('.profile__title');
 const profileInformation = document.querySelector ('.profile__text');
 const profileNameInput = document.querySelector ('.popup__form_type_name');
 const profileInformationInput = document.querySelector ('.popup__form_type_information');
-const popupCloseButton = document.querySelector ('.popup__close');
-const formElement = document.querySelector ('.popup__fieldset');
 const editPopup = document.querySelector ('.popup_edit');
-const editPopupButton = document.querySelector('.profile__edit');
+const formElement = editPopup.querySelector ('.popup__fieldset_edit');
+const editPopupCloseButton = editPopup.querySelector ('.popup__close_edit');
+const editPopupOpenButton = document.querySelector('.profile__edit');
 
 //универсальная функция открытия
 const openPopup = (popup)=>{
@@ -18,76 +18,49 @@ const closePopup = (popup)=> {
   popup.classList.remove('popup_opened');
 };
 
-editPopupButton.addEventListener ('click', () => {
+editPopupOpenButton.addEventListener ('click', () => {
   openPopup(editPopup);
-profileNameInput.value = profileName.textContent;
-profileInformationInput.value = profileInformation.textContent;
+  profileNameInput.value = profileName.textContent;
+  profileInformationInput.value = profileInformation.textContent;
 });
 
-popupCloseButton.addEventListener ('click', () => {
+editPopupCloseButton.addEventListener ('click', () => {
   closePopup(editPopup);
 });
 
-editPopup.addEventListener('submit', (evt) => {
+formElement.addEventListener('submit', (evt) => {
   evt.preventDefault(); 
     profileName.textContent = profileNameInput.value;
     profileInformation.textContent = profileInformationInput.value;
     closePopup(editPopup);
 });
 
-
-//массив карточек 
-const cards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 //добавление массива карточек
 const elementsList = document.querySelector('.elements__list');
-
-const imagePopupOpen = document.querySelector('.popup_open_image');
+const elementsTemplate = elementsList.querySelector('#templateCard');
+const imagePopupOpen = document.querySelector('.popup_openimage');//переменная используется далее, не совсем поняла комментарий
 const imagePopupClose = imagePopupOpen.querySelector('.popup__close_img');
-const fullImage = document.querySelector('.popup__image'); 
+const fullImage = imagePopupOpen.querySelector('.popup__image'); 
 const captionImage = imagePopupOpen.querySelector('.popup__caption');
 
+//метод создания элемента карточки
 const createCard = (card) => {
-  const newCard = document.querySelector('#templateCard').content.cloneNode(true);
-  newCard.querySelector('.elements__img').src = card.link;
+  const newCard = elementsTemplate.content.cloneNode(true);
+  const elementsImg = newCard.querySelector ('.elements__img');
+  elementsImg.src = card.link;
   newCard.querySelector('.elements__text').textContent = card.name;
-
-  newCard.querySelector('.elements__img').addEventListener('click', ()=> {
+  newCard.querySelector('.elements__text').alt = card.name;
+  //посмотреть картинку 
+  elementsImg.addEventListener('click', ()=> {
     openPopup(imagePopupOpen);
-
     fullImage.src = card.link;
     captionImage.textContent = card.name;
     fullImage.setAttribute('alt', card.name);
   });
+  
   imagePopupClose.addEventListener('click', () => {
     closePopup(imagePopupOpen);
   });
-
 //удаление карточки
   const deleteButton = newCard.querySelector('.elements__delete');
   deleteButton.addEventListener('click', (evt) => {
@@ -95,14 +68,19 @@ const createCard = (card) => {
     const card = button.closest('.elements__blok');
     card.remove();
   });
-  
 //лайк
   const likeButton = newCard.querySelector('.elements__like');
   likeButton.addEventListener('click', () =>
   likeButton.classList.toggle('elements__like_active'));
-  elementsList.prepend(newCard);
+
+  return newCard;
 };
-cards.forEach(createCard);
+//метод создания карточки
+const addCard = (card) => {
+  const newCard = createCard(card);
+  elementsList.prepend(newCard);
+}
+cards.forEach(addCard);
 
 //функция создания карточки
 const cardNameInput = document.querySelector ('.popup__form_card_name');
@@ -115,16 +93,16 @@ const addPopupButton = document.querySelector('.profile__add');
 function formAddSubmitHandler (evt) {
 	evt.preventDefault(); 
   const form = evt.target;
-  const link = form.querySelector('.popup__form_card_image').value;
-  const name = form.querySelector('.popup__form_card_name').value;
-  const alt = form.querySelector('.popup__form_card_name').value;
+  const link = cardImageInput.value;
+  const name = cardNameInput.value;
+  const alt = cardNameInput.value;
   const card = {
     name: name,
     link: link,
-    alt: name,
   };
-    createCard(card);
+    addCard(card);
     closePopup(addPopup);
+    form.reset();
 };
 
 addPopupButton.addEventListener ('click', () => {
@@ -133,5 +111,5 @@ addPopupButton.addEventListener ('click', () => {
 popupAddCloseButton.addEventListener ('click', () => {
   closePopup(addPopup);
 });
-formAddElement.addEventListener('submit', formAddSubmitHandler);
 
+formAddElement.addEventListener('submit', formAddSubmitHandler);
