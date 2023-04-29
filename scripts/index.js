0ae7216da895554eb04816254294f84224021316
Gulnara Fedorova <gul-khurmatova@yandex.ru>
2023-04-29 16:53:00 +0200

    6й спринт 1я итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,44 @@
-//редактирование профиля, открытие и закрытие окна редактирования профиля
+//открытие и закрытие окна редактирования профиля
 const profileName = document.querySelector ('.profile__title');
 const profileInformation = document.querySelector ('.profile__text');
 const profileNameInput = document.querySelector ('.popup__form_type_name');
 const profileInformationInput = document.querySelector ('.popup__form_type_information');
 const editPopup = document.querySelector ('.popup_edit');
-const formElement = editPopup.querySelector ('.popup__fieldset_edit');
+const editFormElement = editPopup.querySelector ('.popup__fieldset_edit');
 const editPopupCloseButton = editPopup.querySelector ('.popup__close_edit');
 const editPopupOpenButton = document.querySelector('.profile__edit');
 
 //универсальная функция открытия
 const openPopup = (popup)=>{
   popup.classList.add('popup_opened');
+  document.addEventListener('keydown', closeEscape);
 };
 
 //универсальная функция закрытия
 const closePopup = (popup)=> {
   popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closeEscape);
 };
 
+//закрытие формы через ESC
+function closeEscape(evt){
+  if (evt.key === 'Escape'){
+    const activePopup = document.querySelector('.popup_opened');
+    closePopup(activePopup);
+  };
+};
+
+//закрытие при клике на оверлей
+const popupList =document.querySelectorAll('.popup')
+popupList.forEach(function(popup){
+  popup.addEventListener('mousedown', (evt) =>{
+    if (evt.target.classList.contains('popup')){
+      closePopup(popup);
+    }
+  });
+});
+
+//редактирование профиля
 editPopupOpenButton.addEventListener ('click', () => {
   openPopup(editPopup);
   profileNameInput.value = profileName.textContent;
@@ -28,8 +49,9 @@ editPopupCloseButton.addEventListener ('click', () => {
   closePopup(editPopup);
 });
 
-formElement.addEventListener('submit', (evt) => {
-  evt.preventDefault(); 
+
+editFormElement.addEventListener('submit', (evt) => {
+  evt.preventDefault(); //отключить дефолтную отправку
     profileName.textContent = profileNameInput.value;
     profileInformation.textContent = profileInformationInput.value;
     closePopup(editPopup);
@@ -38,7 +60,7 @@ formElement.addEventListener('submit', (evt) => {
 //добавление массива карточек
 const elementsList = document.querySelector('.elements__list');
 const elementsTemplate = elementsList.querySelector('#templateCard');
-const imagePopupOpen = document.querySelector('.popup_openimage');//переменная используется далее, не совсем поняла комментарий
+const imagePopupOpen = document.querySelector('.popup_openimage');
 const imagePopupClose = imagePopupOpen.querySelector('.popup__close_img');
 const fullImage = imagePopupOpen.querySelector('.popup__image'); 
 const captionImage = imagePopupOpen.querySelector('.popup__caption');
@@ -49,7 +71,7 @@ const createCard = (card) => {
   const elementsImg = newCard.querySelector ('.elements__img');
   elementsImg.src = card.link;
   newCard.querySelector('.elements__text').textContent = card.name;
-  newCard.querySelector('.elements__text').alt = card.name;
+  elementsImg.alt = card.name;
   //посмотреть картинку 
   elementsImg.addEventListener('click', ()=> {
     openPopup(imagePopupOpen);
@@ -81,6 +103,7 @@ const addCard = (card) => {
   elementsList.prepend(newCard);
 }
 cards.forEach(addCard);
+
 
 //функция создания карточки
 const cardNameInput = document.querySelector ('.popup__form_card_name');
