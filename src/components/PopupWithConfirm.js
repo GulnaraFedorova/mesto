ba8a057d708c16b93436bf82466e6871f8051357
Gulnara Fedorova <gul-khurmatova@yandex.ru>
2023-07-22 14:55:25 +0200

    9й спринт 1я итерация

--- src/components/PopupWithConfirm.js
+++ src/components/PopupWithConfirm.js
@@ -1,32 +1,3 @@
-/*import Popup from "./Popup.js";
-
-export default class PopupWithConfirm extends Popup {
-	constructor(popupSelector, handleButtonConfirm) {
-		super(popupSelector);
-        this._form = this._popup.querySelector('.popup__fieldset');
-		this._submitButton = this._form.querySelector('.popup__button_confirmation');
-		this._buttonDefaultValue = this._submitButton.textContent;
-		this._handleButtonConfirm = handleButtonConfirm;
-	}
-
-	setEventListeners() {
-		super.setEventListeners();
-		this._form.addEventListener('click', () => {
-			this._handleButtonConfirm();
-			this._submitButton.textContent = "Удаление..."
-		});
-	}
-	
-	setDefaultButtonText() {
-		this._submitButton.textContent = this._buttonDefaultValue;
-	}
-
-	close() {
-      this._form.reset();
-      super.close();
-    }
-}*/
-
 import Popup from "./Popup.js";
 
 export default class PopupWithConfirm extends Popup {
